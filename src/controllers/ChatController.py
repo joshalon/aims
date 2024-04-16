b640caeef6499e4be19eb3d29fec9ee8aa882b42
Josh Alon <jalon@asu.edu>
2024-04-15 22:29:14 -0700

    added app.yaml file to create entry point for flask server on the google cloud

--- src/controllers/ChatController.py
+++ src/controllers/ChatController.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from openai import OpenAI
 import logging
+import os
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
 client = OpenAI()
@@ -14,20 +17,17 @@ def generate_response():
     messages = data.get('messages', [])
     user_message = messages[-1]['content']
 
-    try:
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful email assistant for Josh and you will be responding as Josh. You will be drafting email responses for users based on given message in their inbox. If you do not know how to respond you will ask for more guidance."},
-                {"role": "user", "content": user_message}
-            ]
-        )
-        response = completion.choices[0].message.content
-        app.logger.info(response)
-        return response
-    except Exception as e:
-        app.logger.error("Error calling OpenAI API: %s", str(e))
-        return "Error calling OpenAI API", 500
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful email assistant for Josh and you will be responding as Josh. You will be drafting email responses for users based on given message in their inbox. If you do not know how to respond you will ask for more guidance."},
+            {"role": "user", "content": user_message}
+        ]
+    )
+
+    response = completion.choices[0].message.content
+    app.logger.info(response)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
